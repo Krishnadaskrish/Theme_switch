08f3bd54430b50bc0dfc801bc3022e720eced3de
hornto <krishnadas10.official@gmail.com>
2023-08-25 10:07:34 +0530

    push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
 
 function App() {
+  const [darkMode, setDarkMode] = useState(true);
+
+  const toggle = () => {
+    setDarkMode(Dark => !Dark);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`container ${darkMode ? 'white' : 'black'}`}>
+      <button onClick={toggle} className="theme-button">
+        Switch Theme
+      </button>
+      <p>
+        Click the button to switch the background theme.
+      </p>
+      
     </div>
+    
   );
 }
 
 export default App;
+
